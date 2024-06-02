5b9537cb3bf7f6b64c38f34b857301dd0e447c4c
parthibanmuthukumar <parthibanmuthukumar3@gmail.com>
2024-06-02 20:02:11 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactdom from "react-dom/client"
+import "./style.css"
+import one from "./img/nature1.jfif"
+import two from "./img/nature2.jfif"
+import three from "./img/download.jfif"
+import four from "./img/download-3.jfif"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = reactdom.createRoot(document.getElementById("root"))
+function Img1() {
+    return(
+<div>
+    <div className="picfol1">
+        <h1 className="topic">IMAGE GALLERY</h1>
+        <div className="border">
+          <div className="one">
+            <img src={one}  alt="pic1"/>
+            <h2>Greenlead Hut</h2>
+          </div>
+          <div className="one">
+            <img src={two}  alt="pic1"/>
+            <h2>Mountain Scenery</h2>
+          </div>
+             <div className="one">
+                <img src={three}  alt="pic1"/>
+                <h2>Sangla Valley</h2>
+             </div>
+             <div className="one">
+                <img src={four}  alt="pic1"/>
+                <h2>Mountain Scenery</h2>
+             </div>
+        </div>
+    </div>
+    <div className="picfol2">
+        <div className="border">
+          <div className="one">
+            <img src={four}  alt="pic1"/>
+            <h2>Mountain Scenery</h2>
+          </div>
+          <div className="one">
+            <img src={one}  alt="pic1"/>
+            <h2>Greenlead Hut</h2>
+          </div>
+             <div className="one">
+                <img src={three}  alt="pic1"/>
+                <h2>Sangla Valley</h2>
+             </div>
+             <div className="one">
+                <img src={two}  alt="pic1"/>
+                <h2>Mountain Scenery</h2>
+             </div>
+        </div>
+    </div>
+</div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<div>
+    <Img1></Img1>
+</div>)
